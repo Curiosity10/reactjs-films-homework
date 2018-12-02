020857965b3439ff131b21c91ebd95fab0913c7a
Curiosity10 <alexds3462@gmail.com>
2018-12-02 14:24:52 +0300

    Update project structure

--- frontend/src/components/Signature/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { hot } from 'react-hot-loader';
-
-import Signature from './Signature';
-
-ReactDOM.render(<Signature name="Dzmitry Aleksandrouski" />, document.getElementById('app') || document.createElement('div'));
-export default hot(module)(Signature);
--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 const express = require('express');
 const app = express();
-const development = process.env.NODE_ENV !== 'production ';
 
-if (development) {
-  const webpack = require('webpack');
-  const webpackConfig = require('./frontend/config/webpack/webpack.dev.config');
-  const webpackDevMiddleware = require('webpack-dev-middleware');
-  const webpackHotMiddleware = require('webpack-hot-middleware');
+switch (process.env.NODE_ENV) {
+  case 'development':
+    const webpack = require('webpack');
+    const webpackConfig = require('./config/webpack/webpack.dev.config');
+    const webpackDevMiddleware = require('webpack-dev-middleware');
+    const webpackHotMiddleware = require('webpack-hot-middleware');
+    const compiler = webpack(webpackConfig);
 
-  const compiler = webpack(webpackConfig);
-  const wpmw = webpackDevMiddleware(compiler, {
-    publicPath: webpackConfig.output.publicPath,
-    stats: { colors: true },
-    logLevel: 'warn',
-  });
-  app.use(wpmw);
+    app.use(webpackDevMiddleware(compiler, {
+      publicPath: webpackConfig.output.publicPath,
+      stats: { colors: true },
+      logLevel: 'warn',
+    }));
 
-  const wphmw = webpackHotMiddleware(compiler, {
-    log: console.log,
-    path: '/__webpack_hmr',
-  });
-  app.use(wphmw);
-  app.use(express.static(`${__dirname}/frontend/build`));
-}
+    app.use(webpackHotMiddleware(compiler, {
+      log: console.log,
+      path: '/__webpack_hmr',
+    }));
 
-if (!development) {
-  const expressStaticGzip = require('express-static-gzip');
-  app.use(expressStaticGzip(`${__dirname}/frontend/build`, {
-    enableBrotli: true,
-  }));
+    app.use(express.static(`${__dirname}/build`));
+    break;
+
+  case 'production':
+    const expressStaticGzip = require('express-static-gzip');
+    app.use(expressStaticGzip(`${__dirname}/build`, {
+      enableBrotli: true,
+    }));
+    break;
 }
 
 const PORT = process.env.PORT || 3000;
