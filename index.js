26f4b402ca9a0465c08c96f633ba64d68dbe6f55
Curiosity10 <alexds3462@gmail.com>
2018-12-03 21:55:28 +0300

    Lint and fix index.js.

--- index.js
+++ index.js
@@ -1,38 +1,50 @@
 const express = require('express');
+const expressStaticGzip = require('express-static-gzip');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const webpackConfig = require('./config/webpack/webpack.dev.config');
+
+const compiler = webpack(webpackConfig);
+
 const app = express();
 
 switch (process.env.NODE_ENV) {
   case 'development':
-    const webpack = require('webpack');
-    const webpackConfig = require('./config/webpack/webpack.dev.config');
-    const webpackDevMiddleware = require('webpack-dev-middleware');
-    const webpackHotMiddleware = require('webpack-hot-middleware');
-    const compiler = webpack(webpackConfig);
+    app.use(
+      webpackDevMiddleware(compiler, {
+        publicPath: webpackConfig.output.publicPath,
+        stats: { colors: true },
+        logLevel: 'warn',
+      }),
+    );
 
-    app.use(webpackDevMiddleware(compiler, {
-      publicPath: webpackConfig.output.publicPath,
-      stats: { colors: true },
-      logLevel: 'warn',
-    }));
-
-    app.use(webpackHotMiddleware(compiler, {
-      log: console.log,
-      path: '/__webpack_hmr',
-    }));
+    app.use(
+      webpackHotMiddleware(compiler, {
+        // eslint-disable-next-line no-console
+        log: console.log,
+        path: '/__webpack_hmr',
+      }),
+    );
 
     app.use(express.static(`${__dirname}/build`));
     break;
 
   case 'production':
-    const expressStaticGzip = require('express-static-gzip');
-    app.use(expressStaticGzip(`${__dirname}/build`, {
-      enableBrotli: true,
-    }));
+    app.use(
+      expressStaticGzip(`${__dirname}/build`, {
+        enableBrotli: true,
+      }),
+    );
+    break;
+
+  default:
     break;
 }
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`🌎 Listening on port ${PORT}!`);
 });
